715a900f2369dda4004096f3e80c5c2aec4149e5
skrytyy <tbduncan99@yahoo.com>
2021-03-25 22:35:30 -0400

    Add files via upload

---
+++ been.py
@@ -0,0 +1,9 @@
+print("Hello been")
+val=input("Enter your Birthday in m/dd format: ")
+birth=("8/18")
+if val == birth:
+    print("Happy Birthday!")
+elif val != birth:
+    print("*unhappys your birthday*")
+
+#thanks for watching been
